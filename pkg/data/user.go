ca8839af0869278b7838952ce1b504eac8730c67
JeremiSavard <jeremi.savard@usherbrooke.ca>
2021-03-23 17:08:27 -0400

    move user data to mock db

--- pkg/data/user.go
+++ pkg/data/user.go
@@ -7,7 +7,7 @@ import (
 var ErrorUserNotFound = fmt.Errorf("User not found")
 
 type User struct {
-	ID           int    `json:"id"`
+	ID           string `json:"id" bson:"_id"`
 	Username     string `json:"username" validate:"required"`
 	Email        string `json:"email" validate:"required,email"`
 	FirstName    string `json:"firstname"`
@@ -21,88 +21,3 @@ type User struct {
 
 // Users is a collection of User
 type Users []*User
-
-//Return every users
-func GetUsers() Users {
-	return userList
-}
-
-// GetUserByID returns a single user with the given id
-func GetUserByID(id int) (*User, error) {
-	index := findIndexByUserID(id)
-	if index == -1 {
-		return nil, ErrorUserNotFound
-	}
-	return userList[index], nil
-}
-
-// AddUser creates a new user
-func AddUser(user *User) {
-	user.ID = getNextID()
-	userList = append(userList, user)
-}
-
-// UpdateUser updates the user specified in received JSON
-func UpdateUser(user *User) error {
-	index := findIndexByUserID(user.ID)
-	if index == -1 {
-		return ErrorUserNotFound
-	}
-	userList[index] = user
-	return nil
-}
-
-// DeleteUser deletes the user with the given id
-func DeleteUser(id int) error {
-	index := findIndexByUserID(id)
-	if index == -1 {
-		return ErrorUserNotFound
-	}
-
-	// This should not work, probably needs ':' after index+1. To test
-	userList = append(userList[:index], userList[index+1])
-
-	return nil
-}
-
-// Returns the index of a user in the database
-// Returns -1 when no user is found
-func findIndexByUserID(id int) int {
-	for index, user := range userList {
-		if user.ID == id {
-			return index
-		}
-	}
-	return -1
-}
-
-//////////////////////////////////////////////////////////////////////////////
-/////////////////////////// Fake database ///////////////////////////////////
-///// DB connection setup and docker file will be done in sprint 8 /////////
-///////////////////////////////////////////////////////////////////////////
-
-func getNextID() int {
-	userList := userList[len(userList)-1]
-	return userList.ID + 1
-}
-
-var userList = []*User{
-	{
-		ID:          1,
-		Username:    "JeremiS",
-		Email:       "jeremi@gmail.com",
-		FirstName:   "Jeremi",
-		LastName:    "Savard",
-		DateOfBirth: "08/02/1996",
-		Gender:      "M",
-	},
-	{
-		ID:          2,
-		Username:    "MalcolmSJ",
-		Email:       "malcolmb@gmail.com",
-		FirstName:   "Malcolm",
-		LastName:    "St-John",
-		DateOfBirth: "01/01/1994",
-		Gender:      "M",
-	},
-}
---
+++ pkg/database/mockDB.go
@@ -0,0 +1,103 @@
+package database
+
+import (
+	"github.com/Ubivius/microservice-user/pkg/data"
+	"github.com/google/uuid"
+)
+
+type MockUsers struct {
+}
+
+func NewMockUsers() UserDB {
+	log.Info("Connecting to mock database")
+	return &MockUsers{}
+}
+
+func (mp *MockUsers) Connect() error {
+	return nil
+}
+
+func (mp *MockUsers) CloseDB() {
+	log.Info("Mocked DB connection closed")
+}
+
+//Return every users
+func (mp *MockUsers) GetUsers() data.Users {
+	return userList
+}
+
+// GetUserByID returns a single user with the given id
+func (mp *MockUsers) GetUserByID(id string) (*data.User, error) {
+	index := findIndexByUserID(id)
+	if index == -1 {
+		return nil, data.ErrorUserNotFound
+	}
+	return userList[index], nil
+}
+
+// AddUser creates a new user
+func (mp *MockUsers) AddUser(user *data.User) error {
+	user.ID = uuid.NewString()
+	userList = append(userList, user)
+	return nil
+}
+
+// UpdateUser updates the user specified in received JSON
+func (mp *MockUsers) UpdateUser(user *data.User) error {
+	index := findIndexByUserID(user.ID)
+	if index == -1 {
+		return data.ErrorUserNotFound
+	}
+	userList[index] = user
+	return nil
+}
+
+// DeleteUser deletes the user with the given id
+func (mp *MockUsers) DeleteUser(id string) error {
+	index := findIndexByUserID(id)
+	if index == -1 {
+		return data.ErrorUserNotFound
+	}
+
+	// This should not work, probably needs ':' after index+1. To test
+	userList = append(userList[:index], userList[index+1])
+
+	return nil
+}
+
+// Returns the index of a user in the database
+// Returns -1 when no user is found
+func findIndexByUserID(id string) int {
+	for index, user := range userList {
+		if user.ID == id {
+			return index
+		}
+	}
+	return -1
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////// Fake database ///////////////////////////////////
+///// DB connection setup and docker file will be done in sprint 8 /////////
+///////////////////////////////////////////////////////////////////////////
+
+var userList = []*data.User{
+	{
+		ID:          "a2181017-5c53-422b-b6bc-036b27c04fc8",
+		Username:    "JeremiS",
+		Email:       "jeremi@gmail.com",
+		FirstName:   "Jeremi",
+		LastName:    "Savard",
+		DateOfBirth: "08/02/1996",
+		Gender:      "M",
+	},
+	{
+		ID:          "e2382ea2-b5fa-4506-aa9d-d338aa52af44",
+		Username:    "MalcolmSJ",
+		Email:       "malcolmb@gmail.com",
+		FirstName:   "Malcolm",
+		LastName:    "St-John",
+		DateOfBirth: "01/01/1994",
+		Gender:      "M",
+	},
+}
