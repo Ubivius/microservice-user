50662bbc14a3441e6743290b27b346df624e0e3a
JeremiSavard <jeremi.savard@usherbrooke.ca>
2021-03-23 17:10:37 -0400

    change put request to match user data

--- scripts/curlRequests.sh
+++ scripts/curlRequests.sh
@@ -5,5 +5,5 @@
 curl localhost:9090/users
 curl localhost:9090/users/a2181017-5c53-422b-b6bc-036b27c04fc8
 curl localhost:9090/users -XPOST -d '{"name":"addName", "price":1.00, "sku":"abc-abc-abcd"}'
-curl localhost:9090/users -XPUT -d '{"id":"a2181017-5c53-422b-b6bc-036b27c04fc8", "name":"newName", "price":2.00, "sku":"abc-abc-abcde"}'
+curl localhost:9090/users -XPUT -d '{"id":"a2181017-5c53-422b-b6bc-036b27c04fc8", "username":"newUSername", "email":"test@email.com", "dateofbirth":"01/01/1970"}'
 curl localhost:9090/users/a2181017-5c53-422b-b6bc-036b27c04fc8 -XDELETE
