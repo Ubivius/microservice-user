0f263e885bbdb728115a3fe96c62b4243651eadc
JeremiSavard <jeremi.savard@usherbrooke.ca>
2021-03-24 15:36:48 -0400

    update scripts

--- scripts/curlRequests.sh
+++ scripts/curlRequests.sh
@@ -4,6 +4,6 @@
 
 curl localhost:9090/users
 curl localhost:9090/users/a2181017-5c53-422b-b6bc-036b27c04fc8
-curl localhost:9090/users -XPOST -d '{"name":"addName", "price":1.00, "sku":"abc-abc-abcd"}'
-curl localhost:9090/users -XPUT -d '{"id":"a2181017-5c53-422b-b6bc-036b27c04fc8", "username":"newUSername", "email":"test@email.com", "dateofbirth":"01/01/1970"}'
+curl localhost:9090/users -XPOST -d '{"id":"a2181017-5c53-422b-b6bc-036b27c04fc8", "username":"username", "email":"test@email.com", "dateofbirth":"01/01/1970"}'
+curl localhost:9090/users -XPUT -d '{"id":"a2181017-5c53-422b-b6bc-036b27c04fc8", "username":"newUSername", "email":"test123@email.com", "dateofbirth":"01/01/1970"}'
 curl localhost:9090/users/a2181017-5c53-422b-b6bc-036b27c04fc8 -XDELETE
