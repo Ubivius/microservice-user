71214d34d4a0934f186858e393eeebc299564a28
malcolm19@hotmail.com <malcolm19@hotmail.com>
2021-11-05 17:26:28 -0400

    client options for mongoDB

--- pkg/database/mongoDB.go
+++ pkg/database/mongoDB.go
@@ -9,6 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
 )
 
 // ErrorEnvVar : Environment variable error
@@ -34,7 +35,9 @@ func (mp *MongoUsers) Connect() error {
 	uri := mongodbURI()
 
 	// Setting client options
-	clientOptions := options.Client().ApplyURI(uri)
+	opts := options.Client()
+	clientOptions := opts.ApplyURI(uri)
+	opts.Monitor = otelmongo.NewMonitor()
 
 	// Connect to MongoDB
 	client, err := mongo.Connect(context.Background(), clientOptions)
