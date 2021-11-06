3a61daa3c4329f95f2f1136da02a6b1c8876163d
malcolm19@hotmail.com <malcolm19@hotmail.com>
2021-11-06 14:05:34 -0400

    integration test setup

--- pkg/database/mongoDB_test.go
+++ pkg/database/mongoDB_test.go
@@ -2,11 +2,34 @@ package database
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/Ubivius/microservice-user/pkg/data"
 	"github.com/google/uuid"
 )
+
+func integrationTestSetup(t *testing.T) {
+	t.Log("Test setup")
+
+	if os.Getenv("DB_USERNAME") == "" {
+		os.Setenv("DB_USERNAME", "admin")
+	}
+	if os.Getenv("DB_PASSWORD") == "" {
+		os.Setenv("DB_PASSWORD", "pass")
+	}
+	if os.Getenv("DB_PORT") == "" {
+		os.Setenv("DB_PORT", "27888")
+	}
+	if os.Getenv("DB_HOSTNAME") == "" {
+		os.Setenv("DB_HOSTNAME", "localhost")
+	}
+
+	err := deleteAllUsersFromMongoDB()
+	if err != nil {
+		t.Errorf("Failed to delete existing items from database during setup")
+	}
+}
 
 func TestMongoDBConnectionAndShutdownIntegration(t *testing.T) {
 	if testing.Short() {
