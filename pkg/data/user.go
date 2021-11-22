e363ceaa8148b4fe8515e840ca05ec96eefad4da
MaximeStOnge <31734216+MaximeStOnge@users.noreply.github.com>
2021-11-22 16:00:22 -0500

    Add Status

--- pkg/data/user.go
+++ pkg/data/user.go
@@ -6,17 +6,29 @@ import (
 
 var ErrorUserNotFound = fmt.Errorf("User not found")
 
+// StatusType of a status
+type StatusType string
+
+// status type of a player
+const (
+	Online  StatusType = "Online"  // user is online
+	Offline StatusType = "Offline" // user is offline
+	InLobby StatusType = "InLobby" // user is in lobby
+	InGame  StatusType = "InGame"  // user is in game
+)
+
 type User struct {
-	ID           string `json:"id" bson:"_id"`
-	Username     string `json:"username" validate:"required"`
-	Email        string `json:"email" validate:"required,email"`
-	FirstName    string `json:"firstname"`
-	LastName     string `json:"lastname"`
-	DateOfBirth  string `json:"dateofbirth"`
-	Gender       string `json:"gender"`
-	Address      string `json:"address"`
-	Bio          string `json:"bio"`
-	Achievements string `json:"achievements"`
+	ID           string     `json:"id" bson:"_id"`
+	Username     string     `json:"username" validate:"required"`
+	Email        string     `json:"email" validate:"required,email"`
+	FirstName    string     `json:"firstname"`
+	LastName     string     `json:"lastname"`
+	DateOfBirth  string     `json:"dateofbirth"`
+	Status       StatusType `json:"status" validate:"isStatusType"`
+	Gender       string     `json:"gender"`
+	Address      string     `json:"address"`
+	Bio          string     `json:"bio"`
+	Achievements string     `json:"achievements"`
 }
 
 // Users is a collection of User
--- pkg/data/user_test.go
+++ pkg/data/user_test.go
@@ -15,3 +15,18 @@ func TestCheckValidation(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestInvalidRelationshipType(t *testing.T) {
+	u := &User{
+		Username:    "JeremiS",
+		Email:       "jeremi@gmail.com",
+		DateOfBirth: "01/01/1999",
+		Status: "Away",
+	}
+
+	err := u.Validate()
+
+	if !(err != nil && err.Error() == "Key: 'User.Status' Error:Field validation for 'Status' failed on the 'isStatusType' tag") {
+		t.Errorf("A status type of value %s passed but StatusType need to be between %s and %s", u.Status, Online, InGame)
+	}
+}
