6324b71832be936ca5ef60f084f8a123b880ccdd
JeremiSavard <jeremi.savard@usherbrooke.ca>
2021-11-22 20:32:07 -0500

    add auth middleware

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Ubivius/microservice-user/pkg/handlers"
 	"github.com/Ubivius/pkg-telemetry/metrics"
+	tokenValidation "github.com/Ubivius/shared-authentication/pkg/auth"
 	"github.com/gorilla/mux"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
 )
@@ -19,6 +20,7 @@ func New(userHandler *handlers.UsersHandler) *mux.Router {
 
 	//Get Router
 	getRouter := router.Methods(http.MethodGet).Subrouter()
+	getRouter.Use(tokenValidation.Middleware)
 	getRouter.HandleFunc("/users", userHandler.GetUsers)
 	getRouter.HandleFunc("/users/{id:[0-9a-z-]+}", userHandler.GetUserByID)
 
@@ -28,16 +30,19 @@ func New(userHandler *handlers.UsersHandler) *mux.Router {
 
 	//Put Router
 	putRouter := router.Methods(http.MethodPut).Subrouter()
+	putRouter.Use(tokenValidation.Middleware)
 	putRouter.HandleFunc("/users", userHandler.UpdateUsers)
 	putRouter.Use(userHandler.MiddlewareUserValidation)
 
 	//Post Router
 	postRouter := router.Methods(http.MethodPost).Subrouter()
+	postRouter.Use(tokenValidation.Middleware)
 	postRouter.HandleFunc("/users", userHandler.AddUser)
 	postRouter.Use(userHandler.MiddlewareUserValidation)
 
 	// Delete router
 	deleteRouter := router.Methods(http.MethodDelete).Subrouter()
+	deleteRouter.Use(tokenValidation.Middleware)
 	deleteRouter.HandleFunc("/users/{id:[0-9a-z-]+}", userHandler.Delete)
 
 	return router
